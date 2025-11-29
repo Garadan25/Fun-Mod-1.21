1967f6a44358d0cc898769428dffa2140a88798f
Garadan25 <ofiramara@gmail.com>
2025-11-29 13:07:14 +0200

    added lemon without textures

--- src/main/java/com/garadan/funthingsmod/FunThingsMod.java
+++ src/main/java/com/garadan/funthingsmod/FunThingsMod.java
@@ -1,6 +1,9 @@
 package com.garadan.funthingsmod;
 
+import com.garadan.funthingsmod.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,6 +32,8 @@ public class FunThingsMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
@@ -41,7 +46,9 @@ public class FunThingsMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
+            event.accept(ModItems.LEMON);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
